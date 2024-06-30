6d3d4dd4c790dd6052330dc62e07d6515334c4c2
ruichu zhang <3032860034@qq.com>
2024-06-30 15:35:42 +0800

    feat:添加保存文件元信息功能

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-cloud-storage
 
 go 1.20
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible // indirect
+	golang.org/x/time v0.5.0 // indirect
+)
--- handler/handler.go
+++ handler/handler.go
@@ -1,22 +1,26 @@
 package handler
 
 import (
+	"go-cloud-storage/meta"
+	"go-cloud-storage/util"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"time"
 )
 
-// 处理文件上传
+// UploadHandler 处理文件上传
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		// 上传html页面
 		data, err := os.ReadFile("./static/index.html")
 		if err != nil {
-			io.WriteString(w, "internal server error")
+			_, _ = io.WriteString(w, "internal server error")
 			return
 		}
-		w.Write(data)
+		_, _ = w.Write(data)
 	} else if r.Method == "POST" {
 		// 接受文件数据流以及存储到本地
 		file, header, err := r.FormFile("fileToUpload")
@@ -24,22 +28,37 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			log.Printf("Failed to get data from request, err:%s\n", err.Error())
 			return
 		}
-		defer file.Close()
-		newFile, err := os.Create("./tem/" + header.Filename)
+		defer func(file multipart.File) {
+			_ = file.Close()
+		}(file)
+		// 获取文件元信息
+		fileMeta := meta.FileMeta{
+			FileName: header.Filename,
+			Location: "./tem/" + header.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+		// 创建本地文件
+		newFile, err := os.Create(fileMeta.Location)
 		if err != nil {
 			log.Printf("Failed to create file, err:%s\n", err.Error())
 			return
 		}
-		defer newFile.Close()
-		_, err = io.Copy(newFile, file)
+		defer func(newFile *os.File) {
+			_ = newFile.Close()
+		}(newFile)
+		// 拷贝文件
+		fileMeta.FileSize, err = io.Copy(newFile, file)
 		if err != nil {
 			log.Printf("Failed to save file, err:%s\n", err.Error())
 			return
 		}
 
+		_, _ = newFile.Seek(0, 0)
+		fileMeta.FileSha1 = util.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
 		http.Redirect(w, r, "/file/upload/success", http.StatusFound)
 	}
 }
 func UploadSuccessHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "upload success")
+	_, _ = io.WriteString(w, "upload success")
 }
